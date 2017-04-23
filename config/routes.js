48343e018a50ea966ea8757a663cc56c9e4b10e1
HKeating <horacekeating@hotmail.co.uk>
2017-04-23 22:36:57 +0100

    adding sessions, problem with validatePassword

--- config/routes.js
+++ config/routes.js
@@ -2,21 +2,27 @@ const express = require('express');
 const router  = express.Router();
 
 // Controllers
-const users = require('../controllers/users');
+const usersController = require('../controllers/users');
+const sessionsController = require('../controllers/sessions');
 
 router.get('/', (req, res) => res.render('statics/home'));
 
 router.route('/users')
-  .get(users.index)
-  .post(users.create);
+  .get(usersController.index)
+  .post(usersController.create);
 router.route('/users/new')
-  .get(users.new);
+  .get(usersController.new);
 router.route('/users/:id')
-  .get(users.show)
-  .put(users.update)
-  .delete(users.delete);
+  .get(usersController.show)
+  .put(usersController.update)
+  .delete(usersController.delete);
 router.route('/users/:id/edit')
-  .get(users.edit);
+  .get(usersController.edit);
+router.route('/login')
+  .get(sessionsController.new)
+  .post(sessionsController.create);
+router.route('/logout')
+  .get(sessionsController.delete);
 
 
 module.exports = router;
--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const morgan          = require('morgan');
 const bodyParser      = require('body-parser');
 const mongoose        = require('mongoose');
 mongoose.promise      = require('bluebird');
+const session         = require('express-session');
 const methodOverride  = require('method-override');
 const env             = require('./config/env');
 const router          = require('./config/routes');
+const User            = require('./models/user');
+
+
 const app             = express();
 
 mongoose.connect(env.db);
@@ -28,6 +32,32 @@ app.use(methodOverride((req) => {
   }
 }));
 
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'shh it\'s a secret',
+  resave: false,
+  saveUninitialized: false
+}));
+
+
+app.use((req, res, next) => {
+  if (!req.session.userId) return next();
+
+  User
+    .findById(req.session.userId)
+    .exec()
+    .then(user => {
+      if (!user) {
+        return req.session.regenerate(() => {
+          // req.flash('danger', 'You must be logged in to view this...');
+          res.redirect('/login');
+        });
+      }
+      res.locals.user = user;
+      res.locals.isLoggedIn = true;
+
+      next();
+    });
+});
 
 app.use(router);
 
