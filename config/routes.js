15a2dd66bf44557cd5bf2dfbf637ac9abb7b2b5f
HKeating <horacekeating@hotmail.co.uk>
2017-04-24 00:41:22 +0100

    set up a users controller for admin

--- config/routes.js
+++ config/routes.js
@@ -4,20 +4,21 @@ const router  = express.Router();
 // Controllers
 const registrationsController = require('../controllers/registrations');
 const sessionsController = require('../controllers/sessions');
+const usersController = require('../controllers/users');
 
 router.get('/', (req, res) => res.render('statics/home'));
 
-// router.route('/users')
-//   .get(usersController.index)
-//   .post(usersController.create);
-// router.route('/users/new')
-//   .get(usersController.new);
-// router.route('/users/:id')
-//   .get(usersController.show)
-//   .put(usersController.update)
-//   .delete(usersController.delete);
-// router.route('/users/:id/edit')
-//   .get(usersController.edit);
+router.route('/users')
+  .get(usersController.index)
+  .post(usersController.create);
+router.route('/users/new')
+  .get(usersController.new);
+router.route('/users/:id')
+  .get(usersController.show)
+  .put(usersController.update)
+  .delete(usersController.delete);
+router.route('/users/:id/edit')
+  .get(usersController.edit);
 router.route('/register')
   .get(registrationsController.new)
   .post(registrationsController.create);
