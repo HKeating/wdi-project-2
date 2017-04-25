b65e3f579ffd047165f01ae15e1139846d2add37
HKeating <horacekeating@hotmail.co.uk>
2017-04-25 11:56:56 +0100

    added drop-downs for topics and subtopics. Still problems with ECONNRESET

--- src/js/client.js
+++ src/js/client.js
@@ -4,14 +4,36 @@ console.log('hello world');
 
 $(init);
 
-const topics = ['Arts', 'Business', 'Comedy', 'Education', 'Games &amp; Hobbies','Government &amp; Organisations', 'Health', 'Kids &amp; Family', 'Music', 'News &amp; Politics', 'Religion &amp; Spirituality', 'Science &amp; Medicine', 'Society &amp; Culture', 'Sports &amp; Hobbies', 'Technology', 'TV &amp; Film'];
+const topics = ['Arts', 'Business', 'Comedy', 'Education', 'Football', 'Games &amp; Hobbies','Government &amp; Organisations', 'Health', 'Kids &amp; Family', 'Music', 'News &amp; Politics', 'Religion &amp; Spirituality', 'Science &amp; Medicine', 'Society &amp; Culture', 'Sports &amp; Hobbies', 'Technology', 'TV &amp; Film'];
+
+const subtopics = {
+  'Arts': ['Design', 'Fashion &amp; Beauty', 'Food', 'Literature', 'Performing Arts', 'Visual Arts'],
+  'Business': ['Business News', 'Careers', 'Investing', 'Management &amp; Marketing', 'Shopping'],
+  'Education': ['Educational Technology', 'Higher Education', 'Language Courses', 'Training', 'K-12'],
+  'Games & Hobbies': ['Automotive', 'Aviation', 'Hobbies', 'Other Games', 'Video Games'],
+  'Government & Organisations': ['Local', 'National', 'Non-Profit', 'Regional'],
+  'Health': ['Alternative Health', 'Fitness &amp; Nutrition', 'Self-Help', 'Sexuality'],
+  'Science & Medicine': ['Medicine', 'Natural Sciences', 'Social Sciences'], 'Society & Culture': ['History', 'Personal Journals', 'Philosophy', 'Places &amp; Travel'],
+  'Sports & Hobbies': ['Amateur', 'University &amp; Secondary School', 'Outdoor', 'Professional'],
+  'Technology': ['Gadgets', 'Podcasting', 'Software How-To', 'Tech News']};
 
 function init() {
-  topicSelector();
+  selectTopic();
 }
 
-function topicSelector() {
+function selectTopic() {
+  const topicSelector = '#topicSelector';
+  const subtopicSelector = '#subtopicSelector';
   topics.forEach(topic => {
-    $(`<option value="${topic}">${topic}</option>`).appendTo('#podcastSelector');
+    $(`<option value="${topic}">${topic}</option>`).appendTo(topicSelector);
+  });
+
+  $(topicSelector).change(() => {
+    $(subtopicSelector).empty();
+    $(`<option value="" disabled selected>Choose a subtopic</option>`).appendTo(subtopicSelector);
+    subtopics[`${$(topicSelector).val()}`].forEach(subtopic => {
+      $(`<option value="${subtopic}">${subtopic}</option>`).appendTo(subtopicSelector);
+    });
+
   });
 }
