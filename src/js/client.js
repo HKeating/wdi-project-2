8c3a68a0feac018b4e9c97e6956b273ed8d9f84e
HKeating <horacekeating@hotmail.co.uk>
2017-04-27 18:11:47 +0100

    working on slick carousel

--- src/js/client.js
+++ src/js/client.js
@@ -31,7 +31,22 @@ function init() {
   $('#description').val('');
   $('#description').trigger('autoresize');
   $('.collapsible').collapsible();
-  $('.carousel').carousel();
+  // $('.carousel').carousel();
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true
+  });
 
 }
 
